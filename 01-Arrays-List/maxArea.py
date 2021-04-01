c6b145017a368acb02e34822358bb27dc8f3cdbf
zhiyo2016@163.com <zhiyo2016@163.com>
2021-04-01 17:38:51 +0800

    update

--- 01-Arrays-List/maxArea.py
+++ 01-Arrays-List/maxArea.py
@@ -4,15 +4,16 @@
 11. 盛最多水的容器
 https://leetcode-cn.com/problems/container-with-most-water/
 """
+from typing import List
+
+
 class Solution:
     def maxArea(self, height: List[int]):
         l, r = 0, len(height)-1
-
         max_area = 0
         while l < r:
-            area = min(height[l],heught(r) * (r-l)
-            max_area = max(max_area,area)
-
+            area = min(height[l],height[r]) * (r-l)
+            max_area = max(max_area, area)
             if height[l] < height[r]:
                 l = l + 1
             else:
@@ -23,3 +24,10 @@ if __name__ == "__main__":
     Solu = Solution()
     result = Solu.maxArea([1,8,6,2,5,4,8,3,7])
     print(result)
+    
+    
+    
+    
+    
+    
+    
