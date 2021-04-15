f316c6c3d5c0124d8273b37d1761da3e6b7d19d9
zhiyo2016@163.com <zhiyo2016@163.com>
2021-04-15 16:42:28 +0800

    update BuyWater.py

--- 01-Arrays-List/BuyWater.py
+++ 01-Arrays-List/BuyWater.py
@@ -1,2 +1,62 @@
+#!/usr/bin/python3
+#-*-coding:utf-8-*-
 
-# https://blog.csdn.net/qq_43086830/article/details/82177880
+class Solution:
+    def BuyWater(self, n):
+        """
+        题目描述：2块钱买一瓶水，2个空瓶可以换一瓶水，3个瓶盖可以换一瓶水,n块钱最终可以得到多少瓶水
+        """
+        self.money = n
+        self.water = 0
+        self.bottle = 0
+        self.cap = 0
+
+        def buy():
+            self.money = self.money - 2
+            self.water = self.water + 1
+            self.bottle = self.bottle + 1
+            self.cap = self.cap + 1
+            if self.money >= 2:
+                buy()
+            if self.cap >=3:
+                self.cap = self.cap -3
+                buy()
+            if self.bottle >= 2:
+                self.bottle = self.bottle - 2
+                buy()
+
+        buy()
+        print(self.water)
+        print(self.money)
+
+    def BuyWater2(self, n):
+        """
+        题目描述：2块钱买一瓶水，3个瓶盖可以换一瓶水,n块钱最终可以得到多少瓶水
+        """
+        self.money = n
+        self.water = 0
+        self.cap = 0
+
+        def buy():
+            self.money = self.money - 2
+            self.water = self.water + 1
+            self.cap = self.cap + 1
+            if self.money >= 2:
+                buy()
+            if self.cap >=3:
+                self.cap = self.cap -3
+                buy()
+        buy()
+        print(self.water)
+        print(self.money)
+if __name__ == "__main__":
+    Solu = Solution()
+    result = Solu.BuyWater2(6)
+    print(result)
+
+
+
+
+
+
+
