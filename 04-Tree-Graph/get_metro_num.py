f0ad3000a8b80b35eb4dbfb50ab072b3d818d596
hiyongz <zhiyo2016@163.com>
2023-11-16 14:28:20 +0800

    更新算法笔记

--- 04-Tree-Graph/get_metro_num.py
+++ 04-Tree-Graph/get_metro_num.py
@@ -9,10 +9,94 @@ from collections import defaultdict
 from collections import deque
 
 
+import os.path
+import random
+
+
+class TestData():
+    # 随机生成一个不重复的列表
+    def creat_list(self,ll, maxn, lines):
+        """
+        :param ll: 生成列表长度
+        :param maxn: 列表中数的项的最大值
+        :lines：代表已有线路
+        :return: 返回 --> 生成的列表
+        """
+        s = []
+        while len(s) < ll:
+            x = random.randint(1, maxn)
+            if x not in s:
+                s.append(x)
+        # print('s', s)
+        return s
+
+
+    def list_to_str(self,ss):
+        sult = ''
+        for i in ss:
+            if sult == '':
+                sult += '%d' % i
+            else:
+                sult += ',%d' % i
+        return sult
+
+
+    def test_case(self,lines, coun, star,src,des):
+        file_path = os.getcwd() + "\\test"
+        if os.path.exists(file_path):
+            os.chdir(file_path)
+
+        for i in range(star, coun + star):
+            filename = 'input%d.txt' % i
+            if os.path.exists(filename):
+                os.remove(filename)
+            fd = open(filename, 'a+')
+            fd.write('src = %d, dest = %d' % (src, des))
+            fd.flush()
+
+            for j in lines:
+                strl = self.list_to_str(j)
+                fd.write('\n')
+                fd.write('%s' % strl)
+                fd.flush()
+
+            fd.close()
+    def generate(self):
+        # 生成lines中的一项
+        # creat_list(10000, 10000)
+        # ll_lines = random.randint(1, 10000)
+        ll_lines = 1
+        lines = []
+        print("--------------------------")
+        # 随机多少条线路
+        # ll = random.randint(1, 10)
+        # maxn = random.randint(1, 100)
+        ll = 10000 # lines条数
+        maxn = 9999 # 最大值
+        # 文件结尾
+        star = 100
+        for m in range(ll):
+            # line_length 每一项长度随机
+            # if m == 1:
+            #     line_length = 600
+            # else:
+            #     line_length = random.randint(5, 1000)
+            line_length = random.randint(1, 1000)
+            line = self.creat_list(line_length, maxn, lines)
+            if line not in lines:
+                lines.append(line)
+        # print('lines', lines)
+        s1, s2 = random.choices(lines, k=2)
+        src, _ = random.choices(s1, k=2)
+        des, _ = random.choices(s2, k=2)
+        # self.test_case(lines, ll_lines, star,src,des)
+        return lines,src,des
+
+
 class Solution():
-    def __init__(self, input_file, output_file):
-        self.input_file = input_file
-        self.output_file = output_file
+    # def __init__(self, input_file, output_file):
+    #     self.input_file = input_file
+    #     self.output_file = output_file
 
     def lines_load(self):
         """加载地铁路线数据
@@ -30,12 +114,12 @@ class Solution():
                     src_dst = [input.strip() for input in line.split(",")]
                     for sd in src_dst:
                         res = [s.strip() for s in sd.split("=")]
-                        if 'src' in res or 'source' in res:
+                        if 'src' in res:
                             src = int(res[1])
-                        elif 'dest' in res or 'target' in res:
+                        elif 'dest' in res:
                             dest = int(res[1])
                 # 铁路线
-                elif 'src' not in line and 'target' not in line and line != '\n':
+                elif 'src' not in line and line != '\n':
                     line2 = eval(line)
                     # lines.append([line2])
                     if isinstance(line2,int):
@@ -130,11 +214,24 @@ class Solution():
 if __name__ == "__main__":
     # input_file = sys.argv[1]
     # output_file = sys.argv[2]
-    input_file = "test/input2.txt"
-    output_file = "test/output2.txt"
-    solu = Solution(input_file, output_file)
-    lines, src, dest, num_of_lines = solu.lines_load()
-    result = solu.get_metro_num_to_dest2(lines, src, dest)
+    # input_file = "test/input2.txt"
+    # output_file = "test/output2.txt"
+    # solu = Solution(input_file, output_file)
+    # lines, src, dest, num_of_lines = solu.lines_load()
+    solu = Solution()
+    d = TestData()
+    ll_lines = 1
+    for i in range(100000):
+        lines, src, dest = d.generate()
+        result = solu.get_metro_num_to_dest2(lines, src, dest)
+        if int(result) >= 2:
+            print("666666666666")
+            d.test_case(lines, ll_lines, i, src, dest)
+            with open("results.txt", "w") as res_txt:
+                res_txt.write(str(result))
+                res_txt.write("\n")
+    print(src)
+    print(dest)
     print(result)
-    assert result == num_of_lines
+    # assert result == num_of_lines
 
